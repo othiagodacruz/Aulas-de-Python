cb6288a409faa4ed1d87a05e4394364d300a5f7c
othiagodacruz <thiagofelipedacruz20@gmail.com>
2025-10-28 14:19:54 -0300

    modified:   Aula 15/DesafioEX15-1.py new file:   Aula 15/DesafioEX15-2.py new file:   Aula 16/DesafioEX16-1.py

---
+++ Aula 15/DesafioEX15-2.py
@@ -0,0 +1,6 @@
+# se a gente não informar o valor iniciar, por padrão assume-se o zero (0)
+# e se não informar o passo, assume-se por padrão o valor de um (1)
+# "for i in (11):" é o exemplo pra contar até 10 de maneira padrão...
+
+for i in range(10, 0, -1):
+    print(i, " ", end="")
