fc4d37f32926e19992879bbcc87c270fef5443d8
othiagodacruz <thiagofelipedacruz20@gmail.com>
2025-10-29 12:38:02 -0300

    modified:   Aula 16/DesafioEX16-1.py 	new file:   Aula 16/DesafioEX16-2.py 	new file:   Aula 16/DesafioEX16-3.py

--- Aula 16/DesafioEX16-2.py
+++ Aula 16/DesafioEX16-2.py
@@ -1,4 +1,4 @@
 for i in range(1,11):
-    if i == 5:
-        continue
+    if i == 7:
+        break
     print(i, "", end="")
---
+++ Aula 16/DesafioEX16-3.py
@@ -0,0 +1,18 @@
+print("Seja bem vindo ao contador de 1 a 100!")
+print("Vamos revisar os comandos...")
+print("Use *Sim* para continuar e *Nao* para parar!")
+
+Nao = float
+Resposta = float
+i = 1
+
+Resposta == Nao
+
+print(i, "", end="")
+
+for i in range(2,100,1):
+    if i == Resposta:
+        break
+    else:
+        print(input("Você deseja continuar!?" , Resposta))
+        print(i, "", end="")
