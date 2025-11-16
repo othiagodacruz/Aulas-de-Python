31b16abaa24336793c71525b48776c1d181f851a
othiagodacruz <thiagofelipedacruz20@gmail.com>
2025-11-16 00:31:53 -0300

    modified:   Aula 7/DesafioEX7-3.py 	modified:   "Relat\303\263rio das Aulas.txt"

--- Aula 7/DesafioEX7-3.py
+++ Aula 7/DesafioEX7-3.py
@@ -2,7 +2,7 @@ dado = 10
 print(f"Conteúdo da Variável dado: {dado}")
 
 dado *= 2 # dado = dado * 1
-print(f"Conteúdo da Variável dado, depois do incremento: {dado}")
+print(f"Conteúdo da Variável dado, depois da multiplicação: {dado}")
 
 dado /= 2 # dado = dado / 1
-print(f"Conteudo da Variável dado, depois do decremento: {dado}")
+print(f"Conteudo da Variável dado, depois do divisão: {dado}")
